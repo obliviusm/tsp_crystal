a7b32bf964947d5b59ac44fa53a6b2ed1716daa7
obliviusm <obliviusm@gmail.com>
2015-10-14 17:32:47 -0400

    parse costs

--- main.cr
+++ main.cr
@@ -3,4 +3,5 @@ require "./services/json_parser.cr"
 
 parser = JSONparser.new "br17"
 initial_solutions = parser.get_initial_solutions
-p initial_solutions
+graph = parser.get_graph_costs
+p graph
--- services/json_parser.cr
+++ services/json_parser.cr
@@ -2,6 +2,7 @@ class JSONparser
   def initialize filename
     f = File.new("data/#{filename}.json")
     @tsp_problem = ((JSON.parse(f) as Hash)["travellingSalesmanProblemInstance"] as Hash)
+    @size = (@tsp_problem["description"] as String).to_i(strict: false)
   end
 
   def get_initial_solutions
@@ -14,5 +15,20 @@ class JSONparser
       initial_solutions.push solution
     end
     initial_solutions
+  end
+
+  def get_graph_costs
+    graph = ([] of Array(Int32))
+    ((@tsp_problem["graph"] as Hash)["vertex"] as Array).each_with_index do |vertex, i|
+      node = Array.new(@size, 9999)
+      ((vertex as Hash)["edge"] as Array).each do |edge|
+        edge = edge as Hash
+        cost = (edge["-cost"] as String).to_f.to_i
+        index = (edge["#text"] as String).to_i
+        node[index] = cost
+      end
+      graph.push node
+    end
+    graph
   end
 end
